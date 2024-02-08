ca976530c49bc406e1972395b987b1bdac50aaf1
Alexey Kuznetsov <leoxander@yandex.ru>
2024-02-08 22:33:50 +0300

    Fixed RecoverExceptionAspect 2

--- src/main/java/edu/alexey/spring/library/web/IndexController.java
+++ src/main/java/edu/alexey/spring/library/web/IndexController.java
@@ -1,5 +1,7 @@
 package edu.alexey.spring.library.web;
 
+import java.nio.channels.IllegalBlockingModeException;
+
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -8,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import edu.alexey.spring.library.aux.RecoverException;
 import lombok.RequiredArgsConstructor;
 
 @Controller("web.IndexController")
@@ -17,12 +20,24 @@ public class IndexController {
 
 	@GetMapping()
 	public String index(Model model) {
+
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		if (!(authentication instanceof AnonymousAuthenticationToken)) {
 			String currentPrincipalName = authentication.getName();
 			model.addAttribute("username", currentPrincipalName);
 		}
+
+		someMethod();
+
+		System.out.println("Возвращено " + void.class.getSimpleName());
+
 		return "index";
+	}
+
+	@GetMapping("/some")
+	@RecoverException
+	public byte someMethod() {
+		throw new IllegalBlockingModeException();
 	}
 
 }
